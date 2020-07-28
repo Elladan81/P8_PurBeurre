3ace984b4f12ac8380d58a68a0615757428c54da
Elladan81 <heladan@hotmail.fr>
2020-07-28 19:06:23 +0200

    This commit update settings.py for deployement. static file for heroku

--- purbeurre/settings.py
+++ purbeurre/settings.py
@@ -132,16 +132,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
-
-STATIC_TMP = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-os.makedirs(STATIC_TMP, exist_ok=True)
-
+# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 # my settings
