95cfed229898cefd3415db06f105dfe7d5304c9e
Elladan81 <heladan@hotmail.fr>
2020-07-28 20:24:51 +0200

    This commit update settings.py for deployement. add allowed host

--- purbeurre/settings.py
+++ purbeurre/settings.py
@@ -27,7 +27,7 @@ POSTGRE_PASSWORD = os.environ.get('PURBEURRE_POSTGRE_PASSWORD')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'oc8purbeurre.herokuapp.com']
 
 # Application definition
 
