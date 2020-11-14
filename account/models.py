4aed1778db118da826500630c437cbdb54a415b8
Elladan81 <heladan@hotmail.fr>
2020-11-14 13:02:02 +0100

    This commit add new class for element in account templates, add crispy template to form, change settings to 'fr' language and update requirements.txt for deploy

--- account/models.py
+++ account/models.py
@@ -4,7 +4,7 @@ from django.conf import settings
 
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    date_of_birth = models.DateField(blank=True, null=True)
+    date_of_birth = models.DateField(blank=True, null=True, verbose_name='Date de Naissance')
     photo = models.ImageField(upload_to='user/%Y/%m/%d', blank=True)
 
     def __str__(self):
