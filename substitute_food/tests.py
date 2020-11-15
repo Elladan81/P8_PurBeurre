c6b23472ad4918f46b168600410dd4bbd120199b
Elladan81 <heladan@hotmail.fr>
2020-11-15 14:27:39 +0100

    This commit add modification for tests.py in substitute_food app

--- substitute_food/tests.py
+++ substitute_food/tests.py
@@ -25,12 +25,11 @@ class TestSearch(TestCase):
         Test one object search
         """
         client = Client()
-        Product.objects.create(product_name="Test")
-        response = client.post(
-            '/search/', {"product_name": "Test"}, follow=True)
-        self.assertContains(response, "Test")
+        obj_test = Product.objects.create(product_name="Test", brands='Test', product_url="http://test.com")
+        response = client.get('/search/', {'product_name': "Test"}, follow=True)
+        self.assertEqual(response.context['products'][0], obj_test)
 
-    def test_find_substitute(self):
+    def test_find_substitute_enough_objects(self):
         """
         Test substitute search in the same category
         """
@@ -39,12 +38,40 @@ class TestSearch(TestCase):
             product_name="Test", product_url="http://test.com")
         obj2 = Product.objects.create(
             product_name="Test2", product_url="http://test2.com")
-        category = Category.objects.create(category_name="categoryTest")
+        obj3 = Product.objects.create(
+            product_name="Test3", product_url="http://test3.com")
+        obj4 = Product.objects.create(
+            product_name="Test4", product_url="http://test4.com")
+        obj5 = Product.objects.create(
+            product_name="Test5", product_url="http://test5.com")
+        category = Category.objects.create(category_name="category_test")
         category.products.add(obj1)
         category.products.add(obj2)
-        response = client.post(
-            '/find_substitute/test/' + str(obj2.id) + '/', follow=True)
-        self.assertEqual(
-            response.context['product_by_category']["categoryTest"][0], obj1)
-        self.assertNotContains(
-            response.context['product_by_category']["categoryTest"], obj2)
+        category.products.add(obj3)
+        category.products.add(obj4)
+        category.products.add(obj5)
+        response = client.get('/find_substitute/test/' + str(obj2.id) + '/', follow=True)
+        self.assertEqual(response.context["product_by_category"]["category_test"][0], obj1)
+        self.assertEqual(response.context["product_by_category"]["category_test"][1], obj3)
+        self.assertEqual(response.context["product_by_category"]["category_test"][2], obj4)
+        self.assertEqual(response.context["product_by_category"]["category_test"][3], obj5)
+        self.assertEqual(len(response.context["product_by_category"]["category_test"]), 4)
+
+    def test_find_substitute_not_enough_objects(self):
+        """
+        Test substitute search in the same category
+        """
+        client = Client()
+        obj1 = Product.objects.create(
+            product_name="Test", product_url="http://test.com")
+        obj2 = Product.objects.create(
+            product_name="PIM", product_url="http://test2.com")
+        obj3 = Product.objects.create(
+            product_name="PAM", product_url="http://test3.com")
+        category = Category.objects.create(category_name="category_test")
+        category.products.add(obj1)
+        category.products.add(obj2)
+        category.products.add(obj3)
+        response = client.get('/find_substitute/test/' + str(obj1.id) + '/', follow=True)
+        self.assertEqual(response.context["product_by_category"]["category_test"][0], obj2)
+        self.assertEqual(response.context["product_by_category"]["category_test"][1], obj3)
